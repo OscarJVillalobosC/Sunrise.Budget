2f1b27f6f70e4f05afcbf14c5855ec224d40f6ec
Oscar Villalobos <oscarjvillalobosc@gmail.com>
2022-01-17 10:28:42 -0500

    updates

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import SignInOutContainer from './components/Login/Containers';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <SignInOutContainer/>
+    </div>
+  );
+}
+
+export default App;
+
